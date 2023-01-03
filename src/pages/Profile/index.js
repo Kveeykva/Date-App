4b4a960ad98a6b9772f9171af85ff62962b03541
Serdar DEV <serdardev2@gmail.com>
2023-01-03 23:22:23 +0300

    Premium content has been added to the profile page, the page structure has been changed. Settings and edit profile button has been created.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ProfilePicture from "../../components/ProfilePicture";
 import { useSelector, useDispatch } from "react-redux";
@@ -8,14 +8,24 @@ import {
   selectCarousel,
 } from "../../redux/Slices/carouselSlice";
 import styles from "./styles";
+import IconButton from "../../components/IconButton";
+import colors from "../../colors";
+import Box from "../../components/Box";
+import Button from "../../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import PremiumContent from "./premiumContent";
 
 const ProfileScreen = (props) => {
+  const navigation = useNavigation();
   const carousel = useSelector((state) => state.carousel.carousel);
   const firstName = carousel.slice(0, 1);
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.inlineContainer}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.inlineContainer}
+      >
         <ProfilePicture />
         <Text style={styles.nameText}>
           {firstName.map((item) => {
@@ -26,7 +36,87 @@ const ProfileScreen = (props) => {
             );
           })}
         </Text>
-      </View>
+        <View>
+          <Text style={styles.completeProfile}>Profili tamamla</Text>
+        </View>
+        <View style={styles.iconView}>
+          <View style={styles.iconTextView}>
+            <View style={styles.littleIcon}>
+              <View>
+                <IconButton
+                  name="settings"
+                  onPress={() => navigation.navigate("Settings")}
+                  color={colors.darkGrey}
+                  size={28}
+                />
+              </View>
+            </View>
+            <Text style={styles.iconText}>Ayarlar</Text>
+          </View>
+          <View style={styles.iconTextView}>
+            <View style={styles.icon}>
+              <IconButton
+                name="pencil"
+                onPress={() => navigation.navigate("EditUserProfile")}
+                color={colors.darkGrey}
+                size={30}
+              />
+            </View>
+            <Text style={styles.iconText}>Profili Düzenle</Text>
+          </View>
+          <View style={styles.iconTextView}>
+            <View style={styles.littleIcon}>
+              <IconButton
+                name="shield"
+                onPress={() => console.warn("Safety")}
+                color={colors.darkGrey}
+                size={28}
+              />
+            </View>
+            <Text style={styles.iconText}>Güvenlik</Text>
+          </View>
+        </View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <View style={styles.boxView}>
+            <View style={styles.premiumBox}>
+              <Box
+                title="Premium"
+                text="Premium üyeliğin sayesinde daha fazla kişiyle tanışabilirsin."
+              />
+              <Button
+                text="Premium'a Yükselt 69.99TL"
+                customStyle={styles.button}
+                textCustomStyle={styles.buttonText}
+              />
+            </View>
+            <View style={styles.boostBox}>
+              <Box
+                title="Boost"
+                text="Boost üyeliğin sayesinde profilinizi daha fazla kişi görebilir. "
+              />
+              <Button
+                text="Boost'a Yükselt 49.99TL"
+                customStyle={styles.boostButton}
+                textCustomStyle={styles.boostButtonText}
+              />
+            </View>
+            <View style={styles.goldBox}>
+              <Box
+                title="Gold"
+                text="Gold üyelik ile bütün özelliklere sahip olabilirsin. Daha fazla kişiyle tanışabilirsin. "
+              />
+              <Button
+                text="Gold'a Yükselt 199.99TL"
+                customStyle={styles.boostButton}
+                textCustomStyle={styles.boostButtonText}
+              />
+            </View>
+          </View>
+        </ScrollView>
+        <View style={styles.premiumContentView}>
+          <PremiumContent />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,19 +1,130 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import colors from "../../colors";
+
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: colors.white,
+    flex: 1,
   },
   inlineContainer: {
-    marginTop: 50,
+    marginTop: 20,
   },
   nameText: {
     fontSize: 20,
     fontWeight: "bold",
     alignSelf: "center",
     marginTop: 10,
+  },
+  completeProfile: {
+    fontSize: 12,
+    alignSelf: "center",
+    marginTop: 10,
+    color: colors.grey,
+    backgroundColor: colors.lightGrey,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+  },
+  iconView: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 20,
+  },
+  iconTextView: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconText: {
+    fontSize: 14,
+    color: colors.darkGrey,
+    marginTop: 5,
+  },
+  icon: {
+    backgroundColor: colors.white,
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    shadowColor: colors.darkGrey,
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 2,
+    shadowRadius: 10,
+    elevation: 10,
+  },
+  littleIcon: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    shadowColor: colors.darkGrey,
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+    shadowOpacity: 2,
+    shadowRadius: 10,
+    elevation: 10,
+  },
+  premiumBox: {
+    marginVertical: 20,
+    backgroundColor: colors.primary,
+    borderRadius: 10,
+    height: 150,
+    width: width - 40,
+    alignSelf: "center",
+    marginRight: 10,
+  },
+  button: {
+    backgroundColor: colors.white,
+    borderRadius: 30,
+    width: 200,
+  },
+  buttonText: {
+    color: colors.darkGrey,
+    fontWeight: "normal",
+  },
+  boostBox: {
+    marginVertical: 20,
+    backgroundColor: colors.turquoise,
+    borderRadius: 10,
+    height: 150,
+    width: width - 40,
+    marginRight: 10,
+  },
+  goldBox: {
+    marginVertical: 20,
+    backgroundColor: colors.gold,
+    borderRadius: 10,
+    height: 150,
+    width: width - 40,
+  },
+  boostButtonText: {
+    color: colors.darkGrey,
+    fontWeight: "normal",
+  },
+  boostButton: {
+    backgroundColor: colors.white,
+    borderRadius: 30,
+    width: 200,
+  },
+  boxView: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginHorizontal: 20,
+  },
+  premiumContentView: {
+    marginHorizontal: 20,
   },
 });
 
